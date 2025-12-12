36126e40ab95dc6b4022b8fd2682e717a45221e0
Wendell-Valentim <wendellldasiilva8@gmail.com>
2025-12-12 13:17:11 -0300

    feat(jwt): Configure JWKSource and JWT signing mechanism for Access Tokens

--- src/main/java/io/github/wendellvalentim/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/wendellvalentim/libraryapi/config/SecurityConfiguration.java
@@ -19,6 +19,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -51,34 +53,29 @@ public class SecurityConfiguration {
                             .loginPage("/login")
                             .successHandler(sucessHandler);
                 })
+                .oauth2ResourceServer(oauth2Rs -> oauth2Rs.jwt(Customizer.withDefaults()))
                 .build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
-    }
 
+    //CONFIGURA O PREFIXO ROLE
     @Bean
     public GrantedAuthorityDefaults grantedAuthorityDefaults () {
         return new GrantedAuthorityDefaults("");
     }
 
-//    //@Bean
-//    public UserDetailsService userDetailsService(UsuarioService usuarioService) {
-////        UserDetails user1 = User.builder()
-////                .username("usuario")
-////                .password(encoder.encode("123"))
-////                .roles("USER")
-////                .build();
-////        UserDetails user2 = User.builder()
-////                .username("admin")
-////                .password(encoder.encode("321"))
-////                .roles("ADMIN")
-////                .build();
-////        return new InMemoryUserDetailsManager(user1, user2);
-//        return new CustomUserDetailService(usuarioService);
-//    }
+    //CONFIGURA, NO TOKEN JWT, O PREFIXO SCOPE
+    @Bean
+    public JwtAuthenticationConverter authenticationConverter () {
+        var authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        authoritiesConverter.setAuthorityPrefix("");
+
+        var converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+
+        return converter;
+    }
+
 
 
 }
